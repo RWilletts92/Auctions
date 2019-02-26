df96b83b668e55a3b8647066ece65022a83347bf
RWilletts92 <43743626+RWilletts92@users.noreply.github.com>
2019-02-26 12:08:29 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,19 +12,12 @@ root = lxml.html.fromstring(html)
 root.cssselect("li p a")
 #
 matchedlinks=root.cssselect("li p a")
-print(matchedlinks)
+#print(matchedlinks)
 
+
+ 
+record={}
 for link in matchedlinks:
   list=link.text_content()
   print(list)
 
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
